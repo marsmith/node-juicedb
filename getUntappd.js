644d3328db57c8bf2523b54fc76bbf741661cd2d
Marty <marsmith@usgs.gov>
2018-05-01 16:53:17 -0400

    add franklin alley

--- getUntappd.js
+++ getUntappd.js
@@ -2,7 +2,7 @@ var localjuice = require('./index');
 
 var untappdVenues = ['troy-discount-beverage/385265', 'olivers-beverage-brew-crew/334214', 'westmere-beverage/58838', 'the-ruck/1797', 'pint-sized/5973790', 'delaware-supply/6894255', 'hill-street-cafe/70543','mohawk-taproom-and-grill/1749228','hunters-on-jay/2265376','hoosick-beverage-center/340342'];
 
-var untappdUsers = ['AlbanyAleandOyster'];
+var untappdUsers = ['AlbanyAleandOyster','FranklinAlleySocialClub'];
 
 untappdVenues.forEach(function (item) {
     localjuice.getUntappdMenu(item).then(function(result){
@@ -26,4 +26,4 @@ untappdUsers.forEach(function (item) {
 });
 
 //for testing
-//localjuice.getUntappdUser('AlbanyAleandOyster')
+//localjuice.getUntappdUser('FranklinAlleySocialClub')
