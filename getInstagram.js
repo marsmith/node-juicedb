eec9020e740c668a5c23b4075004227edbe62c92
Marty <marsmith@usgs.gov>
2018-05-21 10:13:00 -0400

    some additions

--- getInstagram.js
+++ getInstagram.js
@@ -1,6 +1,6 @@
 var localjuice = require('./index');
 
-var instagramUsers = ['troy_beverage','newburghbrewing','westkillbrewing','woodstockbrewing','oxbowbrewingcompany','burlingtonbeer','planbeefarmbrewery','alchemistbeer','peekskillbrewery','rareformbrewco','fiddleheadbrewing','chathambrewing','druthersbrewing','commonrootsbrewing','paradoxbrewery','adirondackbrewery','suarezfamilybrewery','rootandbranchbrewing','foambrewers','tiredhandsbrewing','threesbrewing','hudsonvalleybrewery','mainebeerco','woodlandbeer','kcbcbeer','barrierbrewingco','singlecutbeer','otherhalfnyc','prairieales','nightshiftbeer','bissellbrothers','industrialartsbrewing','lawsonsfinest','treehousebrewco','grimmales','licbeerproject','trilliumbrewing','finbackbrewery','eqbrewery','fobeerco','hillfarmstead','sloopbrewingco']
+var instagramUsers = ['troy_beverage','newburghbrewing','woodstockbrewing','burlingtonbeer','planbeefarmbrewery','alchemistbeer','peekskillbrewery','rareformbrewco','fiddleheadbrewing','chathambrewing','druthersbrewing','commonrootsbrewing','paradoxbrewery','adirondackbrewery','suarezfamilybrewery','rootandbranchbrewing','foambrewers','hudsonvalleybrewery','mainebeerco','kcbcbeer','barrierbrewingco','singlecutbeer','otherhalfnyc','prairieales','nightshiftbeer','bissellbrothers','industrialartsbrewing','lawsonsfinest','treehousebrewco','grimmales','licbeerproject','trilliumbrewing','finbackbrewery','eqbrewery','fobeerco','hillfarmstead','sloopbrewingco','albanyaleandoyster'];
 
 instagramUsers.forEach(function (item) {
     localjuice.instagramByUser(item).then(function(result){
