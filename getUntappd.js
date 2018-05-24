24091c5bd25c5bfb893c0bb72fce346afaed46bb
Marty <marsmith@usgs.gov>
2018-05-24 11:26:22 -0400

    update vendors

--- getUntappd.js
+++ getUntappd.js
@@ -1,6 +1,6 @@
 var localjuice = require('./index');
 
-var untappdVenues = ['troy-discount-beverage/385265', 'olivers-beverage-brew-crew/334214', 'westmere-beverage/58838', 'the-ruck/1797', 'pint-sized/5973790', 'delaware-supply/6894255', 'hill-street-cafe/70543','mohawk-taproom-and-grill/1749228','hunters-on-jay/2265376','hoosick-beverage-center/340342','elixir-16/6362949','the-black-bear-inn/305546','pontoosuc-package-store/4055110','sinclair-saratoga/6356586','sharon-package-store/4220556'];
+var untappdVenues = ['troy-discount-beverage/385265', 'olivers-beverage-brew-crew/334214', 'westmere-beverage/58838', 'the-ruck/1797', 'pint-sized/5973790', 'delaware-supply/6894255', 'hill-street-cafe/70543','mohawk-taproom-and-grill/1749228','hunters-on-jay/2265376','hoosick-beverage-center/340342','elixir-16/6362949','the-black-bear-inn/305546','pontoosuc-package-store/4055110','sinclair-saratoga/6356586','sharon-package-store/4220556','beer-bones/7454563','kellys-package-store/295792','minogues-beverage-center/418856','minogues-beverage-center/448787','henry-street-taproom/432437','pint-sized/6181207'];
 
 var untappdUsers = ['AlbanyAleandOyster','FranklinAlleySocialClub'];
 
