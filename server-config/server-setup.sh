ee8c276768a7034e19b14007db59bbd7794d4a5e
Marty <marsmith@usgs.gov>
2018-09-11 11:50:05 -0400

    updates

--- server-config/server-setup.sh
+++ server-config/server-setup.sh
@@ -2,7 +2,7 @@
 
 #args
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
-APP_PATH="/srv"
+APP_PATH="/home/pi"
 USER=$SUDO_USER
 LIST_OF_MAIN_APPS="git mariadb-client mariadb-server php7.0 php7.0-mysql apache2 libapache2-mod-php7.0 phpmyadmin"
 
@@ -18,6 +18,9 @@ apt-get install -y $LIST_OF_MAIN_APPS
 git clone https://github.com/marsmith/node-localjuicedb ${APP_PATH}
 git clone https://github.com/marsmith/thejuicefeed ${APP_PATH}
 
+#install npm dependencies
+npm install --prefix ${APP_PATH}/node-localjuicedb
+
 #create symbolic link
 ln -s ${APP_PATH}/thejuicefeed /var/www/html/thejuicefeed
 
@@ -25,6 +28,9 @@ ln -s ${APP_PATH}/thejuicefeed /var/www/html/thejuicefeed
 (crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getUntappd.js" ) | crontab -u ${USER} -
 (crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getInstagram.js" ) | crontab -u ${USER} -
 (crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getTwitter.js" ) | crontab -u ${USER} -
+
+#set mysql root password
+mysql_secure_installation
 
 #create blank database
 echo "Please enter root user MySQL password!"
