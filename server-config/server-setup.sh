b962cfd2c102cdd9b55082caca1632f6beb89124
Marty <marsmith@usgs.gov>
2018-09-11 11:16:46 -0400

    updates

--- server-config/server-setup.sh
+++ server-config/server-setup.sh
@@ -2,28 +2,44 @@
 
 #args
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
-APP_PATH="/home/pi"
+APP_PATH="/srv"
 USER=$SUDO_USER
-LIST_OF_MAIN_APPS="nodejs apache2 mariadb-server"
+LIST_OF_MAIN_APPS="git mariadb-client mariadb-server php7.0 php7.0-mysql apache2 libapache2-mod-php7.0 phpmyadmin"
 
-#install nodesource repo
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#universal script to install latest node.js on any raspberry pi version
+wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | bash;
 
 #install apps
 apt-get update  # To get the latest package lists
+#apt-get upgrade #upgrade all softwares
 apt-get install -y $LIST_OF_MAIN_APPS
+
+#download repos
+git clone https://github.com/marsmith/node-localjuicedb ${APP_PATH}
+git clone https://github.com/marsmith/thejuicefeed ${APP_PATH}
 
 #create symbolic link
 ln -s ${APP_PATH}/thejuicefeed /var/www/html/thejuicefeed
 
 #setup up cron jobs
-(crontab -u ${USER} -l; echo "*/5 * * * * ${APP_PATH}/nwis-mapper/server-config/chkCherry.sh" ) | crontab -u ${USER} -
-(crontab -u ${USER} -l; echo "0 0 * * 0 rm -rf ${APP_PATH}/nwis-mapper/mapper/exporter/temp/*" ) | crontab -u ${USER} -
+(crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getUntappd.js" ) | crontab -u ${USER} -
+(crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getInstagram.js" ) | crontab -u ${USER} -
+(crontab -u ${USER} -l; echo "*/10 * * * * /usr/bin/nodejs ${APP_PATH}/node-localjuicedb/getTwitter.js" ) | crontab -u ${USER} -
 
-#add new virtual site
-cp ${APP_PATH}/nwis-mapper/server-config/nwis-mapper.conf /etc/apache2/sites-available/nwis-mapper.conf
-cp ${APP_PATH}/nwis-mapper/server-config/nwis-mapper-ssl.conf /etc/apache2/sites-available/nwis-mapper-ssl.conf
+### create virtual host rules file
+echo "
+    <VirtualHost *:80>
+      ServerName thejuicefeed.com
+      ServerAlias www.thejuicefeed.com
+      DocumentRoot /var/www/html/thejuicefeed
+      ErrorLog ${APACHE_LOG_DIR}/error.log
+      CustomLog ${APACHE_LOG_DIR}/access.log combined
+    </VirtualHost>" > '/etc/apache2/sites/available/thejuicefeed.com.conf'
+echo -e $"\nNew Virtual Host Created\n"
+
+#enable and disable
 a2dissite 000-default
-a2ensite nwis-mapper
-a2ensite nwis-mapper-ssl
+a2ensite thejuicefeed.com
 
+#restart apache2
+systemctl restart apache2 
