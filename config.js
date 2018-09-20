36cbcb386b35a7bd22bb27f6bb00f5cca61123d4
Marty <marsmith@usgs.gov>
2018-09-20 14:31:43 -0600

    add towne tavern

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.instagramUsers = ['troy_beverage','woodstockbrewing','burlingtonbeer','a
 
 exports.twitterUsers = ['TreeHouseBrewCo','burlingtonbeer','riverroostvt','eqbrewery'];
 
-exports.untappdVenues = ['troy-discount-beverage/385265', 'olivers-beverage-brew-crew/334214', 'westmere-beverage/58838', 'the-ruck/1797', 'pint-sized/5973790', 'delaware-supply/6894255', 'hill-street-cafe/70543','mohawk-taproom-and-grill/1749228','hunters-on-jay/2265376','hoosick-beverage-center/340342','elixir-16/6362949','the-black-bear-inn/305546','beer-bones-taproom/7454563','minogues-beverage-center/418856','minogues-beverage-center/448787','delmar-beverage/397052'];
+exports.untappdVenues = ['towne-tavern/63590','troy-discount-beverage/385265', 'olivers-beverage-brew-crew/334214', 'westmere-beverage/58838', 'the-ruck/1797', 'pint-sized/5973790', 'delaware-supply/6894255', 'hill-street-cafe/70543','mohawk-taproom-and-grill/1749228','hunters-on-jay/2265376','hoosick-beverage-center/340342','elixir-16/6362949','the-black-bear-inn/305546','beer-bones-taproom/7454563','minogues-beverage-center/418856','minogues-beverage-center/448787','delmar-beverage/397052'];
 
 exports.untappdUsers = ['AlbanyAleandOyster','FranklinAlleySocialClub'];
